19be34c4986913c7ff1e37cf989269db67b0ffd5
tca <theconartist@gmail.com>
2015-06-10 14:53:01 -0500

    fixed lambda calculus example

--- t/lc.scm
+++ t/lc.scm
@@ -3,17 +3,20 @@
 	(match match))
 
 (define-rewrite-system lc
+  ((eval (quote x) _) --> x)
+
+  ((eval (var a) (env (nil))) --> (error))
   ((eval (var a) (env (c (b a v) e))) --> v)
   ((eval (var a) (env (c _ r))) --> (eval (var v) (env r)))
-  ((eval (var a) (env (nil))) --> (error))
 
   ((eval (lam p b) (env e)) --> (clo e p b))
 
-  ((eval (app (clo e p b) a) (env _)) --> (eval b (env (c (b p a) e)))))
+  ((eval (app f a) e) --> (app1 (eval f e) (eval a e)))
+  ((app1 (clo ce p b) a) --> (eval b (env (c (b p a) ce)))))
 
 (display (lc '(eval (var (a)) (env (nil)))))
 (newline)
 (display (lc '(eval (lam (x) (var (x))) (env (nil)))))
 (newline)
-(display (lc '(eval (app (lam (x) (var (x))) (q)) (env (nil)))))
+(display (lc '(eval (app (lam (x) (var (x))) (quote (q))) (env (nil)))))
 (newline)
